b95ecfd02e83b0529d34925635873ed76c76250a
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2023-06-05 13:28:29 +0300

    minus gh-pages

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import SelectedShop from "./components/Shop/SelectedShop";
 
 export const App = () => {
   return (
-    <HashRouter basename="https://yivgha.github.io/web-shop/">
+    <HashRouter >
       <Routes>
-        <Route exact path="https://yivgha.github.io/web-shop/" element={<Home />}>
+        <Route exact path="/" element={<Home />}>
           
           <Route  path="shop" element={<Shop />}>
             <Route path="/shop/:id" element={<SelectedShop />} />
